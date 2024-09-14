a8b8dafe92b9670b9b3d04b26ed1cc209b6c7841
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-09-15 00:34:28 +0530

    Homepage done

---
+++ client/src/App.jsx
@@ -0,0 +1,81 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register </a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/11/IMG_9861.jpg?resize=1280,960"
+            alt=""
+          />
+        </div>
+
+        <div className="text">
+          <h2>Humane’s Ai Pin up close</h2>
+          <p className="info">
+            <a className="author">Dawid Paszko</a>
+            <time>2023-01-06</time>
+          </p>
+          <p className="summary">
+            We spent 90 minutes with the pin and its founders at Humane’s SF
+            offices
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/11/IMG_9861.jpg?resize=1280,960"
+            alt=""
+          />
+        </div>
+
+        <div className="text">
+          <h2>Humane’s Ai Pin up close</h2>
+          <p className="info">
+            <a className="author">Dawid Paszko</a>
+            <time>2023-01-06</time>
+          </p>
+          <p className="summary">
+            We spent 90 minutes with the pin and its founders at Humane’s SF
+            offices
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/11/IMG_9861.jpg?resize=1280,960"
+            alt=""
+          />
+        </div>
+
+        <div className="text">
+          <h2>Humane’s Ai Pin up close</h2>
+          <p className="info">
+            <a className="author">Dawid Paszko</a>
+            <time>2023-01-06</time>
+          </p>
+          <p className="summary">
+            We spent 90 minutes with the pin and its founders at Humane’s SF
+            offices
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
